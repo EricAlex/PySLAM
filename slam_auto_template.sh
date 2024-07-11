6db3498af71f92659d387c68c3bd27522ecdc4bd
Xin Wang <xin.wang@imotion.ai>
2024-07-11 13:51:32 +0800

    update slam pipeline script

--- slam_auto_template.sh
+++ slam_auto_template.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
-downloadBucket="test-tm-1323001658"
-uploadBucket="test-wx-1323001658"
+downloadBucket="imo-bev-mid-data-1322593371"
+uploadBucket="imo-bev-lane-1322593371"
 dataset="Parking_PC_240510_chery.M3X_6083_SuZhou_1"
-cloudType="local"
+cloudType="cloud"
 tmp_dir="tmp"
 codeBase="PySLAM"
 indoor=1
-subMapFrameNum=10
+subMapFrameNum_outdoor=20
+subMapFrameNum_indoor=80
 
 # # Download data
 
@@ -23,16 +24,25 @@ subMapFrameNum=10
 # SLAM
 
 if [ ${indoor} -ne 0 ]; then # indoor is true
-    python main_icp_slam.py --data_base_dir ../${tmp_dir}/${dataset}/sweeps --num_icp_points 40000 --sub_map_frames ${subMapFrameNum} --indoor
+    python main_icp_slam.py --data_base_dir ../${tmp_dir}/${dataset}/sweeps --num_icp_points 40000 --sub_map_frames ${subMapFrameNum_indoor} --indoor
 else
-    python main_icp_slam.py --data_base_dir ../${tmp_dir}/${dataset}/sweeps --num_icp_points 40000 --sub_map_frames ${subMapFrameNum}
+    python main_icp_slam.py --data_base_dir ../${tmp_dir}/${dataset}/sweeps --num_icp_points 40000 --sub_map_frames ${subMapFrameNum_outdoor}
 fi
 
 # Create map
 
-for map in `ls result/${dataset}/`; do
-    if [ -d result/${dataset}/${map} ]; then
-        python PcAlign.py --camera_calib_dir intrinsic_extrinsic --slam_paras_file_dir result/${dataset}/${map} &
+map_count=0
+for map in result/${dataset}/map*; do
+    if [ -d "${map}" ]; then
+        map_count=$((map_count + 1))
+    fi
+done
+
+echo "map count: " ${map_count}
+
+for map in result/${dataset}/map*; do
+    if [ -d "${map}" ]; then
+        python PcAlign.py --camera_calib_dir intrinsic_extrinsic --slam_paras_file_dir ${map} --map_count ${map_count} &
     fi
 done
 wait
